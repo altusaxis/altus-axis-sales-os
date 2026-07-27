b92ac899bb66be2e625783d85a21d9cd02210749
altusaxis <james@altusaxis.com>
2026-07-27 12:04:10 +0700

    Create next.config.js

---
+++ next.config.js
@@ -0,0 +1,13 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  typescript: {
+    // Allows production builds to complete even if your project has type errors
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    // Allows production builds to complete even if your project has ESLint errors
+    ignoreDuringBuilds: true,
+  },
+};
+
+module.exports = nextConfig;
